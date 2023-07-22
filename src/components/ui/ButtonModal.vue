52d9f0ea4f7bf08d827d8204b4f5bba1d48f25ed
unknown <darknemiroff@gmail.com>
2023-07-22 19:31:57 +0400

    Create HomePage

---
+++ src/components/ui/ButtonModal.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button type="button" class="uppercase text-white bg-gradient-to-r from-blue-500 via-blue-600 
+        to-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center 
+        mr-2 mb-22" :buttonName="buttonName">{{ buttonName }}</button>        
+    </div>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+    buttonName: {
+        type: String,
+        required: true
+    }
+})
+</script>
---
+++ src/views/HomePageView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-gradient-to-r from-teal-100 via-sky-100 to-teal-100 h-screen w-screen flex justify-center items-center">
+        <div class="flex">
+        <ButtonModal buttonName="Register" />
+        <ButtonModal buttonName="Login" />
+        </div>
+    </div>
+</template>
+<script setup> 
+import ButtonModal from '../components/ui/ButtonModal.vue';
+</script>
