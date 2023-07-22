b14c5cfc3337a366b38393f2812374d258f8a431
unknown <darknemiroff@gmail.com>
2023-07-23 01:26:17 +0400

    Register Modal

---
+++ src/components/shared/RegisterModal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="bg-gray-500 h-full rounded-md">
+        <form @submit.prevent="register">
+        <input v-model="userName" className="w-full mb-2 h-10 rounded-md  bg-white" type="text" name="name" placeholder="Name" />
+        <input v-model="email" className="w-full mb-2 h-10 rounded-md  bg-white" type="email" name="email" placeholder="Email" />
+        <input v-model="password" className="w-full mb-2 h-10 rounded-md  bg-white" type="password" name="password" placeholder="Password" />
+        <input v-model="repeatPassword" className="w-full mb-2 h-10 rounded-md  bg-white" type="password" name="password_confirmation" placeholder="Repeat Password" />
+    <button class="bg-red-500" type="submit">Register</button>
+    </form>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+let userName = ref('')
+let password = ref('')
+let repeatPassword = ref('')
+let email = ref('')
+
+function register(){
+    console.log({
+        'name': userName.value,
+        "email": email.value,
+        "password": password.value,
+        "repeatpassword": repeatPassword.value
+    });
+}
+</script>
--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -1,11 +1,19 @@
 <template>
-    <div class="bg-gradient-to-r from-teal-100 via-sky-100 to-teal-100 h-screen w-screen flex justify-center items-center">
+    <div class="relative bg-gradient-to-r from-teal-700 via-sky-700 to-teal-700 h-screen w-screen flex justify-center items-center">
         <div class="flex">
-        <ButtonModal buttonName="Register" />
+        <ButtonModal buttonName="Register" @click="hello" />
         <ButtonModal buttonName="Login" />
+        </div>
+        <div class="absolute w-1/4 h-1/2 z-10">
+        <RegisterModal />
         </div>
     </div>
 </template>
 <script setup> 
-import ButtonModal from '../components/ui/ButtonModal.vue';
+import ButtonModal from '@/components/ui/ButtonModal.vue'
+import RegisterModal from '@/components/shared/RegisterModal.vue'
+
+function hello(){
+    console.log('hello');
+}
 </script>
