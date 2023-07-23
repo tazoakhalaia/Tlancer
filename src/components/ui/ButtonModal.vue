51adb00e82f65d652568aa4bc0b45d191bd203c5
unknown <darknemiroff@gmail.com>
2023-07-23 10:31:14 +0400

    feat/login post request

--- src/components/ui/ButtonModal.vue
+++ src/components/ui/ButtonModal.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <button type="button" class="uppercase text-white bg-gradient-to-r from-blue-500 via-blue-600 
-        to-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center 
-        mr-2 mb-22" :buttonName="buttonName">{{ buttonName }}</button>        
+        to-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-0.5 text-center 
+        ml-2 mb-22" :buttonName="buttonName">{{ buttonName }}</button>        
     </div>
 </template>
 <script setup>
--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -1,12 +1,38 @@
 <template>
-    <div class="relative bg-gradient-to-r from-teal-700 via-sky-700 to-teal-700 h-screen w-screen flex justify-center items-center">
-        <div class="flex">
-        <ButtonModal buttonName="Register" @click="openRegistration" />
-        <ButtonModal buttonName="Login" />
+    <div class="relative h-screen w-screen flex">
+        <div class="w-1/2">
+            <section class="bg-gray-50 dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Sign In and post new articles
+              </h1>
+              <form class="space-y-4 md:space-y-6" >
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input v-model="email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input v-model="password" type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                  </div>
+                  <button @click.prevent="signIn" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-blue-500">Sign in</button>
+                  <div class="text-sm font-light text-gray-500 dark:text-gray-400 flex">
+                      Don’t have an account yet?  <ButtonModal buttonName="Sign up" @click="openRegistration" />
+                  </div>
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
         </div>
             <transition name="registermodal">
                 <RegisterModal  v-if="openRegistrationModal" />
             </transition>
+            <div class="w-1/2 bg-red-500">
+                right
+            </div>
     </div>
 </template>
 <script setup> 
@@ -14,9 +40,22 @@ import { ref } from 'vue';
 import ButtonModal from '@/components/ui/ButtonModal.vue'
 import RegisterModal from '@/components/shared/RegisterModal.vue'
 import '@/assets/css/registrationModalAnimation.css'
+import axios from 'axios';
 let openRegistrationModal = ref(false)
+let email = ref('')
+let password = ref('')
 
 function openRegistration(){
     openRegistrationModal.value = true
+}
+
+async function signIn(){
+    let token;
+    let response = await axios.post('http://exam.tlancer.net/api/login', {
+        "email": email.value,
+        "password": password.value
+    })
+    token = response.data.token
+    localStorage.setItem('token', token)
 }
 </script>
